9c597df95a60a3f696e1871fa5ea934b6a4f7b0f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx
@@ -0,0 +1,44 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<%@ Register Assembly="DevExpress.Web.v15.1, Version=15.1.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+    Namespace="DevExpress.Web" TagPrefix="dx" %>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--region #Markup-->
+<head runat="server">
+    <script type="text/javascript">
+        function OnClickButtonDel(s, e) {
+            grid.PerformCallback('Delete');
+        }
+    </script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <dx:ASPxGridView ID="grid" KeyFieldName="ID" runat="server" AutoGenerateColumns="False"
+        OnCellEditorInitialize="gridView_CellEditorInitialize" OnRowInserting="gridView_RowInserting"
+        OnRowUpdating="gridView_RowUpdating" ClientInstanceName="grid" 
+        OnCustomCallback="gridView_CustomCallback" 
+        ondatabinding="grid_DataBinding" >
+        <Columns>
+            <dx:GridViewCommandColumn VisibleIndex="0" ShowSelectCheckbox="True" ShowEditButton="True" ShowNewButton="True" ShowUpdateButton="True">
+                <FooterTemplate>
+                    <dx:ASPxButton ID="buttonDel" AutoPostBack="false" runat="server" Text="Delete">
+                        <ClientSideEvents Click="OnClickButtonDel"/>
+                    </dx:ASPxButton>
+                </FooterTemplate>
+            </dx:GridViewCommandColumn>
+            <dx:GridViewDataTextColumn FieldName="ID" ReadOnly="True" VisibleIndex="1">
+            </dx:GridViewDataTextColumn>
+            <dx:GridViewDataTextColumn FieldName="Data" VisibleIndex="2">
+            </dx:GridViewDataTextColumn>
+        </Columns>
+        <Settings ShowFooter="True" />
+    </dx:ASPxGridView>
+    </div>
+    </form>
+</body>
+<!--endregion #Markup-->
+</html>
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,85 @@
+﻿Imports System
+Imports System.Data
+Imports DevExpress.Web
+Imports System.Collections.Generic
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Private table As DataTable = Nothing
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        If (Not IsPostBack) AndAlso (Not IsCallback) Then
+            table = New DataTable()
+            table.Columns.Add("ID", GetType(Integer))
+            table.Columns.Add("Data", GetType(String))
+            table.PrimaryKey = New DataColumn() { table.Columns("ID") }
+            For i As Integer = 0 To 9
+                table.Rows.Add(New Object() { i, "row " & i.ToString() })
+            Next i
+            Session("table") = table
+        Else
+            table = DirectCast(Session("table"), DataTable)
+        End If
+    End Sub
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        If (Not IsCallback) AndAlso (Not IsPostBack) Then
+            grid.DataBind()
+        End If
+    End Sub
+    Protected Sub grid_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+        grid.DataSource = table
+    End Sub
+    Protected Sub gridView_RowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataInsertingEventArgs)
+        table = DirectCast(Session("table"), DataTable)
+        Dim gridView As ASPxGridView = DirectCast(sender, ASPxGridView)
+        Dim row As DataRow = table.NewRow()
+        row("ID") = e.NewValues("ID")
+        row("Data") = e.NewValues("Data")
+        gridView.CancelEdit()
+        e.Cancel = True
+        table.Rows.Add(row)
+    End Sub
+    Protected Sub gridView_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+        table = DirectCast(Session("table"), DataTable)
+        Dim row As DataRow = table.Rows.Find(e.Keys(0))
+        row("Data") = e.NewValues("Data")
+        grid.CancelEdit()
+        e.Cancel = True
+    End Sub
+    Protected Sub gridView_CellEditorInitialize(ByVal sender As Object, ByVal e As ASPxGridViewEditorEventArgs)
+        Dim grid As ASPxGridView = DirectCast(sender, ASPxGridView)
+        If e.Column.FieldName = "ID" Then
+            Dim textBox As ASPxTextBox = CType(e.Editor, ASPxTextBox)
+            textBox.ClientEnabled = False
+            If grid.IsNewRowEditing Then
+                table = DirectCast(Session("table"), DataTable)
+                textBox.Text = GetNewId().ToString()
+            End If
+        End If
+    End Sub
+#Region "#CustomCallbackMethod"
+    Protected Sub gridView_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+        If e.Parameters = "Delete" Then
+            table = DirectCast(Session("table"), DataTable)
+            Dim selectItems As List(Of Object) = grid.GetSelectedFieldValues("ID")
+            For Each selectItemId As Object In selectItems
+                table.Rows.Remove(table.Rows.Find(selectItemId))
+            Next selectItemId
+            grid.DataBind()
+            grid.Selection.UnselectAll()
+        End If
+    End Sub
+#End Region ' #CustomCallbackMethod
+  Private Function GetNewId() As Integer
+        table = DirectCast(Session("table"), DataTable)
+        If table.Rows.Count = 0 Then
+            Return 0
+        End If
+        Dim max As Integer = Convert.ToInt32(table.Rows(0)("ID"))
+        For i As Integer = 1 To table.Rows.Count - 1
+            If Convert.ToInt32(table.Rows(i)("ID")) > max Then
+                max = Convert.ToInt32(table.Rows(i)("ID"))
+            End If
+        Next i
+        Return max + 1
+  End Function
+End Class
